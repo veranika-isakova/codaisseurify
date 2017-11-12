a20937049c97e98fe364bfe63970b7b056f1f9c3
VERANIKA <veranika.isakova89@gmail.com>
2017-11-12 13:15:02 +0100

    Add Model Specs: Songs and Artists

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a name" do
+      artist = Artist.new(name: nil)
+      artist.valid?
+      expect(artist.errors).to have_key(:name)
+    end
+
+    it "is invalid without a country" do
+      artist = Artist.new(country: nil)
+      artist.valid?
+      expect(artist.errors).to have_key(:country)
+    end
+
+    it "is invalid with a biography longer than 1000 characters" do
+      artist = Artist.new(biography: "a"*1001)
+      artist.valid?
+      expect(artist.errors).to have_key(:biography)
+    end
+    it "is invalid without an official site" do
+      artist = Artist.new(official_site: nil)
+      artist.valid?
+      expect(artist.errors).to have_key(:official_site)
+    end
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a song name" do
+      song = Song.new(song_name: "")
+      song.valid?
+      expect(song.errors).to have_key(:song_name)
+    end
+
+    it "is invalid without an album_name" do
+      song = Song.new(album_name: nil)
+      song.valid?
+      expect(song.errors).to have_key(:album_name)
+    end
+    it "is invalid without a date of the release" do
+      song = Song.new(release_date: nil)
+      song.valid?
+      expect(song.errors).to have_key(:release_date)
+    end
+  end
 end
