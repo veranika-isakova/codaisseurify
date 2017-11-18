f640c3ed3fed41a38b5e5570429539f19f65d30d
VERANIKA <veranika.isakova89@gmail.com>
2017-11-18 16:56:59 +0100

    Remove songs related to an artist

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,26 @@
 //= require jquery
 //= require bootstrap-sprockets
+
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function() {
+  $("a.delete_song").bind('click', deleter);
+});
+
+function deleter() {
+  var delete_song = this;
+  var songId = $(delete_song).parent().attr('id');
+  deleteSong(songId);
+}
+
+function deleteSong(songId) {
+  $.ajax({
+    type: "DELETE",
+    url: "/api/songs/" + songId,
+    contentType: "application/json",
+    dataType: "json"
+  })
+  .done(function(data) {
+    $('div#'+songId).remove();
+  });
+}
