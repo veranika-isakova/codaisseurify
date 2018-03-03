3a9dca98a100a19293069fb817f7fef8b761d8f2
VERANIKA <veranika.isakova89@gmail.com>
2018-03-03 13:00:51 +0100

    Add screenshot for readME

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :artists
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
